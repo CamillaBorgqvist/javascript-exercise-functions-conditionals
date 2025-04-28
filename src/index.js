84d18adc330a9db7d1e0e672088cb0fd7dc45091
Camilla Borgqvist <camilla.borgqvist@banqsoft.com>
2025-04-28 19:21:34 +0200

    exercises completed

--- src/index.js
+++ src/index.js
@@ -6,23 +6,42 @@ When you've set that up, invoke the function to see if it works.
 */
 
 const theCoders = (driver, navigator) => {
-    console.log(
-      `The awesome coders for today is the driver ${driver} and the navigator ${navigator}`
-    );
-  };
-  theCoders("jennie", "tom");
+    console.log(`The awesome coders for today is the driver ${driver} and the navigator ${navigator}`)
+  }
+
+  theCoders("Jennie", "Alfons");
+
   /*
   2)
   Pass in only ONE name when you call the function and see what happens.
   (HINT, you should get undefined...)
   */
-  
+  const coders = (driver, navigator) => {
+    console.log(`The awesome coders for today is the driver ${driver} and the navigator ${navigator}`)
+  }
+
+  coders("Jennie");
+
   /*
   3) 
   create a function that checks which name is the longest when comparing the coders 
   and print out: "The driver has the longest name." or "The navigator has the longest name" 
   or if the names ar the same length "The driver and the navigator has equally long names"
   */
+
+  const compareName = (driver, navigator) => {
+
+    if (driver.length === navigator.length){
+      console.log("The driver and the navigator has equally long names")
+    }
+    else if (driver.length > navigator.length){
+      console.log("The driver has the longest name")
+    }
+    else if (driver.length < navigator.length){
+      console.log("The navigator has the longest name")
+    }
+  }
+  compareName("Jan", "Janne");
   
   /*
   4) 
@@ -34,8 +53,24 @@ const theCoders = (driver, navigator) => {
   or alert('23! Younger than Zlatan') or something like that.
   Add as many conditonals as you want.
   */
-  
-  const calculationTest = () => {
+
+  const checkUserAge = () => {
+
+    let userInputAge = prompt ("Type your age to see if you can watch the movie.")
+
+    if (userInputAge <= 15){
+      alert("You're just a kid. Not this movie!")
+    }
+    else if (userInputAge > 15 && userInputAge <= 30 ){
+      alert("Perfect age, you will like this movie!")
+    }
+    else if (userInputAge > 30){
+      alert("You're a bit older but you can watch the movie aswell.")
+    }
+  }
+
+  checkUserAge ()
+
     /*
     5)
     In this function we want to give the user a mathematical calculation to 
@@ -44,7 +79,24 @@ const theCoders = (driver, navigator) => {
     If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
     You choose if to use addition, subtraction, multiplication or division. 
     */
-  };
+    let firstNumber = 2;
+    let secondNumber = 3;
+
+    const calculationTest = () => {
+      let userAnswer = Number(prompt (`What is ${firstNumber} + ${secondNumber}?`));
+
+      let sum = firstNumber + secondNumber
+      
+      if (sum === userAnswer){
+        console.log ("Yay, good job with the calculation!")
+      }
+      else if (sum !== userAnswer){
+        console.log(`Sorry. The correct answer is ${sum}`)
+      }
+        
+    }
+
+  calculationTest ()
   
   /*
   6) **BONUS**
